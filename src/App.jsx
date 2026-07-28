47449553dab0d1e3cd480dc819d7e3f53909387e
ajay0959 <ajay2007nagesh@gmail.com>
2026-07-28 13:58:25 +0530

    day_12_React

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import Footer from './components/Footer/Footer'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <h1>Hello </h1>
+    <Footer/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import './Footer.css'
+
+function Footer(){
+    const year = new Date().getFullYear();
+    return(
+        <footer className='site-footer'>
+            <p>
+                &copy;{year}Ajay Kumar.N .Built with React🌐
+            </p>
+        </footer>
+    )
+}
+export default Footer; // We must export
